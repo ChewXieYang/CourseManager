aa61724976f106c32f1d4c1891c8d95d78588a02
Chew Xie Yang <1221304859@student.mmu.edu.my>
2024-01-03 23:16:13 +0800

    Add files via upload

---
+++ Courses.java
@@ -0,0 +1,15 @@
+public class Courses {
+    private int Credit;
+    private String Code;
+    private String PreReq;
+
+    public Course(int Credit, String Code, String PreReq){
+        this.Credit = Credit;
+        this.Code = Code;
+        this.PreReq = PreReq;
+    }
+
+    public int getCredit(){
+        return Credit;
+    }
+}
