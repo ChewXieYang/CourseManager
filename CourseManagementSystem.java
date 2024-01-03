f1682d69e74855db47a4b71adfd89633a8d87390
Chew Xie Yang <1221304859@student.mmu.edu.my>
2024-01-03 22:39:54 +0800

    Update CourseManagementSystem.java

--- CourseManagementSystem.java
+++ CourseManagementSystem.java
@@ -1,5 +1,9 @@
-
+import java.util.*;
 
 public class CourseManagementSystem{
+    public static void main(String[] args) {
+        System.out.println("Welcome to Course Manager");
+        System.out.println("Courses");
 
+    }
 }
