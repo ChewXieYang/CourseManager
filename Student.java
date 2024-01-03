24ec6636d29ea6e0081b652d336a6afd05e4756c
Chew Xie Yang <1221304859@student.mmu.edu.my>
2024-01-03 22:39:02 +0800

    Update Student.java

--- Student.java
+++ Student.java
@@ -1,3 +1,17 @@
 public class Student {
+    private int ID;
+    private String Name;
     
+    public Student(int ID, String Name){
+        this.ID = ID;
+        this.Name = Name;
+    }
+
+    public int getID(){
+        return ID;
+    }
+
+    public String toString(){
+        return ID + " " + Name;
+    }
 }
