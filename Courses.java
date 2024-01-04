2647350296fe246bd2a098993bbaf13f03fe645e
Chew Xie Yang <1221304859@student.mmu.edu.my>
2024-01-05 00:21:27 +0800

    Update Courses.java

--- Courses.java
+++ Courses.java
@@ -1,3 +1,6 @@
+import java.io.*;
+import java.util.*;
+
 public class Courses {
     private int Credit;
     private String Code;
@@ -17,5 +20,30 @@ public class Courses {
     }
     public String getPreReq(){
         return PreReq;
+    }
+}
+
+class CourseFile{
+    public static void main(String[] args){
+    String CourseFile = "Courses.csv";
+
+    try{
+    Scanner scanner = new Scanner(new File(CourseFile));
+    LinkedList<String[]> CourseData = new LinkedList<>();
+
+    while(scanner.hasNextLine()){
+        String[] line = scanner.nextLine().split(",");
+        CourseData.add(line);
+    }
+
+    for (String[] row: CourseData){
+        for(String value : row){
+            System.out.print(value + "\t");
+        }
+        System.out.println();
+    }
+    scanner.close();
+    } catch(FileNotFoundException e){
+        e.printStackTrace();
     }
 }
